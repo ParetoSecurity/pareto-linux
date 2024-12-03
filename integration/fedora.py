d66f1eb3e9d9e09ade6f09ff84c0274280d34f5f
Janez T <hey@dz0ny.dev>
2024-12-03 21:17:09 +0100

    Install Debian packages in integration scripts for Ubuntu and Debian

--- integration/fedora.py
+++ integration/fedora.py
@@ -1,3 +1,4 @@
 vm.wait_for_unit("multi-user.target")
 print(vm.succeed("ls -all /mnt/package"))
+vm.succeed("sudo dnf install -y /mnt/package/*x86_64.rpm")
 vm.succeed("exit 0")
--- integration/ubuntu.py
+++ integration/ubuntu.py
@@ -1,3 +1,4 @@
 vm.wait_for_unit("multi-user.target")
 print(vm.succeed("ls -all /mnt/package"))
+vm.succeed("sudo dpkg -i /mnt/package/*amd64.deb")
 vm.succeed("exit 0")
