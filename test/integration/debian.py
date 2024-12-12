086a812efcf83c8869c85dfd270ed0305869f60c
Janez T <hey@dz0ny.dev>
2024-12-12 16:07:20 +0100

    Update integration tests to use apt for package installation and add auditor check for failures

--- test/integration/debian.py
+++ test/integration/debian.py
@@ -1,3 +1,7 @@
 vm.wait_for_unit("multi-user.target")
 print(vm.succeed("ls -all /mnt/package"))
 vm.succeed("sudo apt install /mnt/package/*amd64.deb")
+
+res = vm.succeed("auditor check --json")
+fail_count = res.count("fail")
+assert fail_count == 0, f"Found {fail_count} failed checks"
