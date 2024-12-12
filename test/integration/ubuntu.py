d17d3bc52c8ba2c7ab18dfb6ccd7ae0429cf20a4
Janez T <hey@dz0ny.dev>
2024-12-12 19:07:59 +0100

    Update integration tests to install specific package version non-interactively

--- test/integration/ubuntu.py
+++ test/integration/ubuntu.py
@@ -1,7 +1,8 @@
 vm.wait_for_unit("multi-user.target")
 print(vm.succeed("ls -all /mnt/package"))
-vm.succeed("sudo apt install /mnt/package/*amd64.deb")
-
+vm.succeed(
+    "DEBIAN_FRONTEND=noninteractive sudo -E apt install /mnt/package/paretosecurity_amd64.deb -y"
+)
 res = vm.succeed("auditor check --json")
 fail_count = res.count("fail")
 assert fail_count == 0, f"Found {fail_count} failed checks"
