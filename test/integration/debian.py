4db38984c995ccd1ee09ff7eb04a80f82c03ba82
Janez T <hey@dz0ny.dev>
2024-12-12 16:06:22 +0100

    Remove concurrency group from GitHub Actions workflow and update Debian integration test to use apt for package installation

--- test/integration/debian.py
+++ test/integration/debian.py
@@ -1,4 +1,3 @@
 vm.wait_for_unit("multi-user.target")
 print(vm.succeed("ls -all /mnt/package"))
-vm.succeed("sudo dpkg -i /mnt/package/*amd64.deb")
-vm.succeed("exit 0")
+vm.succeed("sudo apt install /mnt/package/*amd64.deb")
