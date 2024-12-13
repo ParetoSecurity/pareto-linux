ba0a3d7f84a8b1037c1da284ad849a5713f5e17e
Janez T <hey@dz0ny.dev>
2024-12-13 14:52:30 +0100

    Add root check and logging for kernel parameters validation

--- checks/kernel.go
+++ checks/kernel.go
@@ -3,6 +3,9 @@ package checks
 import (
 	"os/exec"
 	"strings"
+
+	"github.com/caarlos0/log"
+	"paretosecurity.com/auditor/shared"
 )
 
 type KernelParamsCheck struct {
@@ -23,12 +26,24 @@ func (k *KernelParamsCheck) FailedMessage() string {
 }
 
 func (k *KernelParamsCheck) Run() error {
+
+	if k.RequiresRoot() && !shared.IsRoot() {
+		log.Debug("Running check via helper")
+		// Run as root
+		passed, err := shared.RunCheckViaHelper(k.UUID())
+		if err != nil {
+			log.WithError(err).Warn("Failed to run check via helper")
+			return err
+		}
+		k.passed = passed
+		return nil
+	}
+
 	params := map[string]string{
-		"net.ipv4.conf.all.rp_filter": "1",
-		"net.ipv4.tcp_syncookies":     "1",
-		"kernel.randomize_va_space":   "2",
-		"fs.protected_hardlinks":      "1",
-		"fs.protected_symlinks":       "1",
+		"net.ipv4.tcp_syncookies":   "1",
+		"kernel.randomize_va_space": "2",
+		"fs.protected_hardlinks":    "1",
+		"fs.protected_symlinks":     "1",
 	}
 	k.passed = true
 	for param, expected := range params {
@@ -57,7 +72,7 @@ func (k *KernelParamsCheck) ReportIfDisabled() bool {
 }
 
 func (k *KernelParamsCheck) UUID() string {
-	return "kernel-params-check"
+	return "cbf2736b-72df-43e3-8789-8eb676ff9014"
 }
 
 func (k *KernelParamsCheck) Status() string {
