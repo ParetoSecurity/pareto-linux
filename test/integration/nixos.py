c599c2c5e29918d7cc9e5ae337e38a539f3e1825
Janez T <hey@dz0ny.dev>
2024-12-16 19:29:08 +0100

    test: Enhance NixOS integration tests to check for dial errors and specific failure counts

--- test/integration/nixos.py
+++ test/integration/nixos.py
@@ -2,4 +2,8 @@ assert "Pareto Security CLI" in machine.succeed("auditor --help")
 
 res = machine.succeed("auditor check --json")
 fail_count = res.count("fail")
-assert fail_count == 0, f"Found {fail_count} failed checks"
+dial_error_count = res.count("Failed to connect to helper")
+assert (
+    dial_error_count == 0
+), f"Helper could not start, found : {dial_error_count} calls to dial error"
+assert fail_count == 4, f"Found {fail_count} failed checks"
