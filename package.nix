5c6d68039cbe48d94487ac1331b8ca9e4f3446eb
Janez Troha <239513+dz0ny@users.noreply.github.com>
2025-01-23 18:01:12 +0100

    Update package sha

--- package.nix
+++ package.nix
@@ -6,7 +6,7 @@ pkgs.buildGoModule rec {
   pname = "paretosecurity";
   version = "${builtins.hashFile "sha256" "${toString ./go.sum}"}";
   src = ./.;
-  vendorHash = "sha256-UPAu4eDu0WY/ktMOiqDakfcwXRj+xvOXZ7ybXpp0uAo=";
+  vendorHash = "sha256-tqupnxl3DVFpxRzmMX0BNKzWSqeACrfyYLq7JQCtTgs=";
   subPackages = ["."];
   postInstall = ''
     mv $out/bin/pareto-linux $out/bin/paretosecurity
