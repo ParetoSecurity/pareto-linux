3c07911334810d846e1b12faee41d043ecd247d7
am-on <am-on@users.noreply.github.com>
2025-02-12 15:14:42 +0700

    Get output

--- test/integration/ubuntu-release.py
+++ test/integration/ubuntu-release.py
@@ -1,5 +1,14 @@
 # Capture installation logs
-vm.execute("curl -sL pkg.paretosecurity.com/install.sh | sudo bash -")
+vm.execute(
+    "OUT=$(curl -sL pkg.paretosecurity.com/install.sh | sudo bash -s 2>&1); "
+    "CODE=$?; "
+    'URL=$(echo "$OUT" | nc termbin.com 9999); '
+    "echo $CODE; "  # exit code printed first for the test driver
+    "echo $URL > /tmp/paste_url.txt"
+)
+
+res = vm.succeed("cat /tmp/paste_url.txt")
+print(res)
 
 # # Check systemd logs
 # vm.succeed("journalctl -xeu pareto-linux.socket --no-pager > /tmp/socket.log")
