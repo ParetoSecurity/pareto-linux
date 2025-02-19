558ecf97913b221d76eb86bb4e2e82d4a3a86591
Janez T <hey@dz0ny.dev>
2025-02-19 22:03:30 +0100

    feat: more tests

--- package.nix
+++ package.nix
@@ -8,6 +8,7 @@ pkgs.buildGo124Module rec {
   src = ./.;
   vendorHash = "sha256-bCbykrAUqAhandPNAABbGfMewIPU1ojxVlvPenYtK38=";
   subPackages = ["."];
+  doCheck = true;
   postInstall = ''
     mv $out/bin/pareto-core $out/bin/paretosecurity
   '';
